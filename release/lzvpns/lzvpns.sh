2a2ce07702d85d488f57eaddf33d097292b29e68
larsonzh <larsonzhang@gmail.com>
2022-08-31 18:25:47 +0800

    modified:   release/lzvpns/lzvpns.sh

--- release/lzvpns/lzvpns.sh
+++ release/lzvpns/lzvpns.sh
@@ -8,6 +8,18 @@
 
 # ------ User parameter configuration area ------
 
+# The client accesses the WAN port of the router from the outside through the domain name or IP address
+# 0--Primary WAN (Default), 1--Secondary WAN
+WAN_ACCESS_PORT=0
+
+#The VPN client accesses the router exit of the WAN through the router
+# 0--Primary WAN (Default), 1--Secondary WAN
+VPN_CLIENT_WAN_PORT=0
+
+# Polling time to detect VPN client access
+# 1~10s (The default is 5 seconds)
+POLLING_TIME=5
+
 # ------------ global variable area -------------
 
 # ---------------- Function area ----------------
