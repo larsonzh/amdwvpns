7706a29efc6043d64c91295951d2eae100bf88c9
larsonzh <larsonzhang@gmail.com>
2022-09-01 11:02:17 +0800

    modified:   release/lzvpns/interface/lzvpnse.sh

--- release/lzvpns/interface/lzvpnse.sh
+++ release/lzvpns/interface/lzvpnse.sh
@@ -8,12 +8,10 @@
 
 # -------- lzvpns.sh transfer data area ---------
 # ---------- Don't manually modify !!! ----------
-WAN_ACCESS_PORT=0
 VPN_WAN_PORT=0
 POLLING_TIME=5
 WAN0=100
 WAN1=200
-IP_RULE_PRIO_HOST=999
 IP_RULE_PRIO_VPN=998
 OVPN_SUBNET_IP_SET="lzvpns_ovpn_subnet"
 PPTP_CLIENT_IP_SET="lzvpns_pptp_client"
