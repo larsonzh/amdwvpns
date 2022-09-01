fbbded3ba20286aa228d7df24fd7bf328feee539
larsonzh <larsonzhang@gmail.com>
2022-09-01 11:10:28 +0800

    modified:   release/lzvpns/lzvpns.sh

--- release/lzvpns/lzvpns.sh
+++ release/lzvpns/lzvpns.sh
@@ -212,14 +212,13 @@ transfer_parameters() {
 	sed -i "s:SYSLOG_FILE=.*$:SYSLOG_FILE=\""${SYSLOG_FILE}"\":g" ${PATH_INTERFACE}/${VPN_EVENT_INTERFACE_SCRIPTS} > /dev/null 2>&1
 }
 
-enable_wan_access_port() {
-	if [ "${WAN_ACCESS_PORT}" = "0" -o "{$WAN_ACCESS_PORT}" = "1" ]; then
-		local access_wan=${WAN0}
-        [ "${WAN_ACCESS_PORT}" = "1" ] && access_wan=${WAN1}
-        local router_local_ip="$( ifconfig br0 | grep "inet addr:" | awk -F: '{print $2}' | awk '{print $1}' 2> /dev/null )"
-        ip rule add from all to "${router_local_ip}" table "${access_wan}" prio "${IP_RULE_PRIO_HOST}" > /dev/null 2>&1
-        ip rule add from "${router_local_ip}" table "${access_wan}" prio "${IP_RULE_PRIO_HOST}" > /dev/null 2>&1
-	fi
+set_wan_access_port() {
+	[ "${WAN_ACCESS_PORT}" != "0" -a "{$WAN_ACCESS_PORT}" != "1" ] && return
+    local router_local_ip="$( ifconfig br0 | grep "inet addr:" | awk -F: '{print $2}' | awk '{print $1}' 2> /dev/null )"
+    local access_wan=${WAN0}
+    [ "${WAN_ACCESS_PORT}" = "1" ] && access_wan=${WAN1}
+    ip rule add from all to "${router_local_ip}" table "${access_wan}" prio "${IP_RULE_PRIO_HOST}" > /dev/null 2>&1
+    ip rule add from "${router_local_ip}" table "${access_wan}" prio "${IP_RULE_PRIO_HOST}" > /dev/null 2>&1
 }
 
 start_service() {
@@ -248,7 +247,7 @@ do
     check_file || break
     [ "${1}" = "stop" ] && stop_run && break
     transfer_parameters
-    enable_wan_access_port
+    set_wan_access_port
     start_service
     create_event_interface "${BOOTLOADER_FILE}" "${PATH_LZ}" "${MAIN_SCRIPTS}"
     create_event_interface "${VPN_EVENT_FILE}" "${PATH_INTERFACE}" "${VPN_EVENT_INTERFACE_SCRIPTS}"
