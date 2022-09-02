b24d5ccbfbea979e3012137a66e5f2260df1f56e
larsonzh <larsonzhang@gmail.com>
2022-09-02 12:22:55 +0800

    modified:   release/lzvpns/lzvpns.sh

--- release/lzvpns/lzvpns.sh
+++ release/lzvpns/lzvpns.sh
@@ -118,7 +118,7 @@ clear_daemon() {
     }
     ipset -q destroy "${VPN_DAEMON_IP_SET_LOCK}"
     ps | grep "${VPN_DAEMON_SCRIPTS}" | grep -v grep | awk '{print $1}' | xargs kill -9 > /dev/null 2>&1
-    [ "${1}" != "1" ] && echo $(date) [$$]: The running VPN daemon of this script in the system has been cleared. | tee -ai "${SYSLOG_FILE}" 2> /dev/null \
+    [ "${1}" != "1" ] && echo $(date) [$$]: The running VPN daemon of this script in the system has been cleared. | tee -ai "${SYSLOG_FILE}" 2> /dev/null
 }
 
 clear_time_task() {
