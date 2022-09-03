31bae252bfff75abc41485b247e26f2c2e333300
larsonzh <larsonzhang@gmail.com>
2022-09-03 23:27:31 +0800

    modified:   release/lzvpns/interface/lzvpnse.sh

--- release/lzvpns/interface/lzvpnse.sh
+++ release/lzvpns/interface/lzvpnse.sh
@@ -16,9 +16,27 @@ OVPN_SUBNET_IP_SET="lzvpns_ovpn_subnet"
 PPTP_CLIENT_IP_SET="lzvpns_pptp_client"
 IPSEC_SUBNET_IP_SET="lzvpns_ipsec_subnet"
 SYSLOG="/tmp/syslog.log"
+TRANSDATA=">>>>>>>>"
 # ---------- Don't manually modify !!! ----------
 # -----------------------------------------------
 
+
+get_trsta() { echo "$( echo "${TRANSDATA}" | awk -F '>' '{print $"'"${1}"'"}' )"; }
+
+get_transdata() {
+    VPN_WAN_PORT="$( get_trsta "1" )"
+    WAN0="$( get_trsta "2" )"
+    WAN1="$( get_trsta "3" )"
+    IP_RULE_PRIO_VPN="$( get_trsta "4" )"
+    OVPN_SUBNET_IP_SET="$( get_trsta "5" )"
+    PPTP_CLIENT_IP_SET="$( get_trsta "6" )"
+    IPSEC_SUBNET_IP_SET="$( get_trsta "7" )"
+    SYSLOG="$( get_trsta "8" )"
+}
+
 lzdate() { echo "$( date +"%F %T" )"; }
+
+
+get_transdata
 
 # END
