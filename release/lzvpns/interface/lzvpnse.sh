35ff976bd2797f6dd27648512e150ae68908d0fe
larsonzh <larsonzhang@gmail.com>
2022-09-05 15:10:24 +0800

    modified:   release/lzvpns/interface/lzvpnse.sh

--- release/lzvpns/interface/lzvpnse.sh
+++ release/lzvpns/interface/lzvpnse.sh
@@ -6,6 +6,11 @@
 
 # BEIGIN
 
+PATH_TMP="${0%/*}"
+[ "${PATH_TMP:0:1}" != '/' ] && PATH_TMP="$( pwd )${PATH_TMP#*.}"
+PATH_TMP="${PATH_TMP%/*}/tmp"
+VPN_DATA_FILE="lzvpnsd.dat"
+
 # ------------- Data Exchange Area --------------
 TRANSDATA=">>>>>>>>"
 # -----------------------------------------------
@@ -13,6 +18,7 @@ TRANSDATA=">>>>>>>>"
 get_trsta() { echo "$( echo "${TRANSDATA}" | awk -F '>' '{print $"'"${1}"'"}' )"; }
 
 get_transdata() {
+	[ "${TRANSDATA}" ] || return 1
     [ -n "$( echo "${TRANSDATA}" | grep -E '^[>]|[>][>]' )" ] && return 1
     VPN_WAN_PORT="$( get_trsta "1" )"
     WAN0="$( get_trsta "2" )"
@@ -24,10 +30,34 @@ get_transdata() {
     SYSLOG="$( get_trsta "8" )"
 }
 
+get_exta() { echo "$( echo "${1}" | awk -F '>' 'NR=="'"${2}"'" {print $1}' )"; }
+
+get_exdata() {
+	[ ! -f "${PATH_TMP}/${VPN_DATA_FILE}" ] && return 1
+	local data_buf="$( cat "${PATH_TMP}/${VPN_DATA_FILE}" 2> /dev/null )"
+	[ "${data_buf}" ] || return 1
+	VPN_WAN_PORT="$( get_exta "${data_buf}" "1" )"
+	WAN0="$( get_exta "${data_buf}" "2" )"
+	WAN1="$( get_exta "${data_buf}" "3" )"
+	IP_RULE_PRIO_VPN="$( get_exta "${data_buf}" "4" )"
+	OVPN_SUBNET_IP_SET="$( get_exta "${data_buf}" "5" )"
+	PPTP_CLIENT_IP_SET="$( get_exta "${data_buf}" "6" )"
+	IPSEC_SUBNET_IP_SET="$( get_exta "${data_buf}" "7" )"
+	SYSLOG="$( get_exta "${data_buf}" "8" )"
+	return 0
+}
+
+get_data() {
+	get_exdata && return 0
+	get_transdata && return 0
+	return 1
+}
+
+
 lzdate() { echo "$( date +"%F %T" )"; }
 
 
-get_transdata || {
+get_data || {
     VPN_WAN_PORT=0
     WAN0=100
     WAN1=200
@@ -37,5 +67,7 @@ get_transdata || {
     IPSEC_SUBNET_IP_SET="lzvpns_ipsec_subnet"
     SYSLOG="/tmp/syslog.log"
 }
+
+
 
 # END
