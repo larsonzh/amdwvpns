3fd76d6c57bc396be1b7501509bbefd428bd59fe
larsonzh <larsonzhang@gmail.com>
2022-09-06 13:13:36 +0800

    modified:   release/lzvpns/interface/lzvpnse.sh

--- release/lzvpns/interface/lzvpnse.sh
+++ release/lzvpns/interface/lzvpnse.sh
@@ -12,11 +12,11 @@ PATH_TMP="${PATH_TMP%/*}/tmp"
 VPN_DATA_FILE="lzvpnsd.dat"
 
 PATH_LOCK="/var/lock"
-LOCK_FILE="${PATH_LOCK}/lzvpns.lock"
+LOCK_FILE="${PATH_LOCK}/lz_rule.lock"
 LOCK_FILE_ID=555
 
 # ------------- Data Exchange Area --------------
-TRANSDATA=">>>>>>>>"
+TRANSDATA=">>>>>>>>>"
 # -----------------------------------------------
 
 get_trsta() { echo "${TRANSDATA}" | awk -F '>' '{print $"'"${1}"'"}'; }
@@ -24,14 +24,15 @@ get_trsta() { echo "${TRANSDATA}" | awk -F '>' '{print $"'"${1}"'"}'; }
 get_transdata() {
 	[ "${TRANSDATA}" ] || return 1
     [ -n "$( echo "${TRANSDATA}" | grep -E '^[>]|[>][>]' )" ] && return 1
-    VPN_WAN_PORT="$( get_trsta "1" )"
-    WAN0="$( get_trsta "2" )"
-    WAN1="$( get_trsta "3" )"
-    IP_RULE_PRIO_VPN="$( get_trsta "4" )"
-    OVPN_SUBNET_IP_SET="$( get_trsta "5" )"
-    PPTP_CLIENT_IP_SET="$( get_trsta "6" )"
-    IPSEC_SUBNET_IP_SET="$( get_trsta "7" )"
-    SYSLOG="$( get_trsta "8" )"
+    LZ_VERSION="$( get_trsta "1" )"
+    VPN_WAN_PORT="$( get_trsta "2" )"
+    WAN0="$( get_trsta "3" )"
+    WAN1="$( get_trsta "4" )"
+    IP_RULE_PRIO_VPN="$( get_trsta "5" )"
+    OVPN_SUBNET_IP_SET="$( get_trsta "6" )"
+    PPTP_CLIENT_IP_SET="$( get_trsta "7" )"
+    IPSEC_SUBNET_IP_SET="$( get_trsta "8" )"
+    SYSLOG="$( get_trsta "9" )"
 }
 
 get_exta() { echo "${1}" | awk -F '>' 'NR=="'"${2}"'" {print $1}'; }
@@ -40,14 +41,15 @@ get_exdata() {
 	[ ! -f "${PATH_TMP}/${VPN_DATA_FILE}" ] && return 1
 	local data_buf="$( cat "${PATH_TMP}/${VPN_DATA_FILE}" 2> /dev/null )"
 	[ "${data_buf}" ] || return 1
-	VPN_WAN_PORT="$( get_exta "${data_buf}" "1" )"
-	WAN0="$( get_exta "${data_buf}" "2" )"
-	WAN1="$( get_exta "${data_buf}" "3" )"
-	IP_RULE_PRIO_VPN="$( get_exta "${data_buf}" "4" )"
-	OVPN_SUBNET_IP_SET="$( get_exta "${data_buf}" "5" )"
-	PPTP_CLIENT_IP_SET="$( get_exta "${data_buf}" "6" )"
-	IPSEC_SUBNET_IP_SET="$( get_exta "${data_buf}" "7" )"
-	SYSLOG="$( get_exta "${data_buf}" "8" )"
+    LZ_VERSION="$( get_exta "${data_buf}" "1" )"
+	VPN_WAN_PORT="$( get_exta "${data_buf}" "2" )"
+	WAN0="$( get_exta "${data_buf}" "3" )"
+	WAN1="$( get_exta "${data_buf}" "4" )"
+	IP_RULE_PRIO_VPN="$( get_exta "${data_buf}" "5" )"
+	OVPN_SUBNET_IP_SET="$( get_exta "${data_buf}" "6" )"
+	PPTP_CLIENT_IP_SET="$( get_exta "${data_buf}" "7" )"
+	IPSEC_SUBNET_IP_SET="$( get_exta "${data_buf}" "8" )"
+	SYSLOG="$( get_exta "${data_buf}" "9" )"
 	return 0
 }
 
@@ -72,6 +74,7 @@ lzdate() { eval echo "$( date +"%F %T" )"; }
 set_lock
 
 get_data || {
+    LZ_VERSION=v0.0.1
     VPN_WAN_PORT=0
     WAN0=100
     WAN1=200
@@ -81,6 +84,9 @@ get_data || {
     IPSEC_SUBNET_IP_SET="lzvpns_ipsec_subnet"
     SYSLOG="/tmp/syslog.log"
 }
+
+echo "$(lzdate)" [$$]: | tee -ai "${SYSLOG}" 2> /dev/null
+echo "$(lzdate)" [$$]: Running LZ VPNS Event Handling Process "${LZ_VERSION}" | tee -ai "${SYSLOG}" 2> /dev/null
 
 
 
