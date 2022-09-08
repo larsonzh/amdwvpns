5243a39c5aee83ea7ca752a80b8d80c2b7c53b96
larsonzh <larsonzhang@gmail.com>
2022-09-08 20:20:09 +0800

    modified:   release/lzvpns/uninstall.sh

--- release/lzvpns/uninstall.sh
+++ release/lzvpns/uninstall.sh
@@ -10,6 +10,7 @@
 # BEIGIN
 
 LZ_VERSION=v0.0.1
+TIMEOUT=10
 CURRENT_PATH="${0%/*}"
 [ "${CURRENT_PATH:0:1}" != '/' ] && CURRENT_PATH="$( pwd )${CURRENT_PATH#*.}"
 SYSLOG="/tmp/syslog.log"
@@ -21,6 +22,24 @@ echo "  LZ ${LZ_VERSION} uninstall script starts running..." | tee -ai "${SYSLOG
 echo "  By LZ (larsonzhang@gmail.com)" | tee -ai "${SYSLOG}" 2> /dev/null
 echo "  $(lzdate)" | tee -ai "${SYSLOG}" 2> /dev/null
 echo ----------------------------------------------------------------- | tee -ai "${SYSLOG}" 2> /dev/null
+
+! read -r -n1 -t ${TIMEOUT} -p "  Automatically terminate after ${TIMEOUT}s, continue? [Y/N] " ANSWER \
+    || [ -n "${ANSWER}" ] && echo -e "\r"
+case ${ANSWER} in
+    Y | y)
+    {
+        echo ----------------------------------------------------------------- | tee -ai "${SYSLOG}" 2> /dev/null
+    }
+    ;;
+    *)
+    {
+        echo ----------------------------------------------------------------- | tee -ai "${SYSLOG}" 2> /dev/null
+        echo "  LZ script uninstallation failed." | tee -ai "${SYSLOG}" 2> /dev/null
+        echo -e "  $(lzdate)\n" | tee -ai "${SYSLOG}" 2> /dev/null
+        exit 1
+    }
+    ;;
+esac
 
 if [ ! -f "${CURRENT_PATH}/lzvpns.sh" ]; then
     echo "$(lzdate)" [$$]: "${CURRENT_PATH}/lzvpns.sh" does not exist. | tee -ai "${SYSLOG}" 2> /dev/null
